8c6b39765f68eb8c86ed0fbf44fc5106091df342
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-11-28 10:46:27 -0600

    Create read02.sh

---
+++ read02.sh
@@ -0,0 +1,27 @@
+Readings: Network scanning with NMAP
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+For Lecture & Lab
+What is a Port Scanner and How Does it Work? A port scanner is a computer program that checks network ports for one of three possible statuses – open, closed, or filtered. It sends a network request to connect to a specific TCP or UDP port on a computer and records the response.
+What is a port? Describe it with an analogy that would help a family member understand.  A virtual location where networking communication starts and ends. 
+What does a port scanner send to a port to check the current status? A port scanner sends a TCP or UDP network packet and asks the port about their current status.
+When a port scanner sends a request to connect, what are the three possible responses? Describe them. Open, Accepted: The computer responds and asks if there is anything it can do for you.
+Closed, Not Listening: The computer responds that “This port is currently in use and unavailable at this time.”
+Filtered, Dropped, Blocked: The computer doesn’t even bother to respond.
+What is the difference between TCP and UDP? Tcp complete with error checking, verification, and a 3-way handshake to confirm each packet is successful. UDP doesn’t have any error checking but tends to be faster. Live streaming and online video games often use UDP for this reason
+Answers cited from https://www.varonis.com/blog/port-scanning-techniques
+
+
+Ports
+List and describe the ports used for the following:
+Telnet telcommunication network protocol 23
+SSH secure shell 22
+DNS domain name system 53
+SMTP simple mail transfer protocol 25
+HTTP hypertext transfer protocol 80
+HTTPS hypertext transfer protocol secure 443
+RDP remote desktop protocol 3389
+Ping
