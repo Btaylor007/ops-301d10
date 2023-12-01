7a55c208a7620c266f5fd144a02ac31fd5c50caa
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-11-30 20:41:19 -0600

    read04.sh

---
+++ read04.sh
@@ -0,0 +1,17 @@
+Readings: Routing
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+VirtualBox Network Settings Guide
+
+Which network mode in VirtualBox can be used to emulate unplugging the Ethernet cable from the network?
+Which network mode would be best if you wanted to run a server on a VM that could be fully accessible from your physical local area network?
+What are the three options of promiscuous mode and what does each do?
+What is Port Forwarding?
+Videos
+Network Topologies
+Routing Technologies
+Dynamic Routing
+Network Switching Overview
