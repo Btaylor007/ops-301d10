87caece5a89bcaee1204c98f18644b2920b24675
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-01 10:54:41 -0600

    read05.sh

--- read05.sh
+++ read05.sh
@@ -10,3 +10,5 @@ What is a site-to-site VPN?  a connection set up between multiple networks
 What is TCP/IP and what is it used for? how data is organized according to packets, given addresses, transmitted, and received across different computers on the internet
 What are some popular reasons for using a VPN? wanting to mask their IP addresses and surf the internet more safely
 Explain the difference between the three types of VPNs.
+
+source cited https://www.fortinet.com/fr/resources/cyberglossary/what-is-site-to-site-vpn
