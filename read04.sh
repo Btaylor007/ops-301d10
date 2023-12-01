5f9c8736b17ff215475023e28d65efe13afa1679
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-11-30 20:54:34 -0600

    read04.sh

--- read04.sh
+++ read04.sh
@@ -6,12 +6,14 @@ Review the Submission Instructions for guidance on completing and submitting thi
 Reading
 VirtualBox Network Settings Guide
 
-Which network mode in VirtualBox can be used to emulate unplugging the Ethernet cable from the network?
+Which network mode in VirtualBox can be used to emulate unplugging the Ethernet cable from the network?Nat Not attached
 Which network mode would be best if you wanted to run a server on a VM that could be fully accessible from your physical local area network?
-What are the three options of promiscuous mode and what does each do?
-What is Port Forwarding?
+What are the three options of promiscuous mode and what does each do? deny- no traffic not intended to the network adapter (default) allow vms lets in all traffic hidden from the network adapter except transmitted to and from other vm
+allow all no restrictions in the mode adapter can see all traffic 
+What is Port Forwarding? used to configure access from the host machine and other hosts of the same physical network to the services running on the guest OS
 Videos
 Network Topologies
 Routing Technologies
 Dynamic Routing
 Network Switching Overview
+Anwsers cited from https://www.nakivo.com/blog/virtualbox-network-setting-guide/
