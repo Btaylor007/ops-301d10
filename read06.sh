cf86dbab3d97f9713ac83811e3d612098ef28584
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-04 10:50:38 -0600

    read06.sh

---
+++ read06.sh
@@ -0,0 +1,16 @@
+Readings: Network Address Translation
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+Network Address Translation (NAT)
+
+What is the main purpose for implementing NAT on a network?If NAT runs out of addresses, i.e., no address is left in the pool configured then the packets will be dropped and an Internet Control Message Protocol (ICMP) host unreachable packet to the destination is sent
+At what layer of the OSI model does NAT happen?
+What happens to packets when NAT runs out of addresses in the pool of available IPs?If NAT runs out of addresses, i.e., no address is left in the pool configured then the packets will be dropped and an Internet Control Message Protocol (ICMP) host unreachable packet to the destination is sent
+What disadvantage does using NAT pose for routers?
+Videos
+Network Address Translation (NAT)
+Common Network Types
+IPv4 and IPv6 Addressing
