53a24404b4d5910748d6e987f28145d5bdc74667
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-05 10:52:08 -0600

    read07.sh

---
+++ read07.sh
@@ -0,0 +1,16 @@
+Readings: Web Server Deployment
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+What is NGINX?
+
+What are some common use cases for NGINX?
+How does NGINX handle tasks that could slow down the web server?
+Describe, as if to a non-technical friend how to actually pronounce “NGINX”, and why an org might chooose to use it.
+Videos
+Network Architectures
+Network Devices
+Network Connectors
+Ethernet Standards
