049bb482eff2f7683439a610bd03261f72edff43
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-06 23:19:35 -0600

    read09.sh

---
+++ read09.sh
@@ -0,0 +1,16 @@
+Readings: Traffic Mirroring
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+How to capture network traffic? SPAN vs TAP
+
+What are the differences between SPAN and TAP? SPAN copies network traffic without affecting the data flow. Configured through the switch's management interface. TAP physically breaks the network connection and inserts itself in the path. Requires a dedicated hardware device.
+What types of network devices can support network traffic mirroring? switches 
+How can network traffic mirroring be used for network security? mirrored traffic should be stored securely and only accessed by authorized personnel.
+Are there any legal or ethical considerations when using network traffic mirroring? It is important to carefully consider the legal and ethical implications of using network traffic mirroring before deploying it in your environment.
+Videos
+Logs and Monitoring
+
+
