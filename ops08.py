a90b5da198a97cdf54faf85b196ecbeb3cd252b3
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-06 22:36:30 -0600

    Create ops08.py

---
+++ ops08.py
@@ -0,0 +1,14 @@
+#list
+fruits = ["apple", "banana", "cherry", "orange", "mango", "grapefruit", "kiwi", "melon", "pineapple", "strawberry"]
+
+# Print the fourth of the list
+print(fruits[3])  # output: orange
+
+# Print the sixth through ten  of the list
+print(fruits[5:])  # output: ['grapefruit', 'kiwi', 'melon', 'pineapple', 'strawberry']
+
+# Change the value of the seven to "onion"
+fruits[6] = "onion"
+
+# Print the modified list
+print(fruits)  # output: ['apple', 'banana', 'cherry', 'orange', 'mango', 'grapefruit', 'onion', 'melon', 'pineapple', 'strawberry']
