bfce9c1bc43e32d7493f9335d98b781f367ca133
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-08 10:53:38 -0600

    read10.sh

---
+++ read10.sh
@@ -0,0 +1,14 @@
+Readings: VPC
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+After you’ve completed the reading, answer the following:
+
+How can one host within a VPC any services that need to be public?
+What are examples of services that would live in the publicly-accessible part of the VPC? The privately-accessible part? Web servers: Websites, web applications, and APIs that need to be accessed by users on the internet.
+Load Balancers: Distributing traffic among multiple instances of a public service. Databases: Storing sensitive data that should not be publicly accessible.
+Internal APIs: APIs used by other services within the VPC but not accessible to the public.
+Management tools: Server management tools for controlling and monitoring resources.
+CDN (Content Delivery Network): Caching static content for faster delivery to users worldwide.
+What are the trade-offs of using a VPC vs traditional infrastructure? Scalability,Easy hybrid cloud deployment,Better performance,Better security
+Reading
+What is a Virtual Private Cloud(VPC)Scalability:
