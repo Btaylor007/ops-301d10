142c8ced793a46c07650fc233ad1b512ec006927
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-07 22:56:09 -0600

    ops07.py

--- ops07.py
+++ ops07.py
@@ -9,3 +9,16 @@ import os
 user_input_path = input("Enter a directory path: ")
 
 # Declaration of functions
+
+def generate_directory_info(path):
+    for (root, directories, files) in os.walk(path):
+        print(f"\n== Root directory: {root} ==")
+        print(f"Directories: {directories}")
+        print(f"Files: {files}")
+
+
+# Get user input
+user_path = input("Enter a directory path: ")
+
+# Generate directory information
+generate_directory_info(user_path)
