2524a9aa046dd4fc736a106274ebe93bb8c1de0b
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-11 10:50:40 -0600

    read10a.sh

---
+++ read10a.sh
@@ -0,0 +1,10 @@
+Readings: Windows Server
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+What’s the Difference Between Windows and Windows Server?
+What is a server, and how is it different from a regular computer? How would you describe this difference to a friend who doesn’t know much about computers? A regular computer runs general things while the server runs the all the big processes. I would say the server is like a library and the computer is like which book you pick out.
+How does the way Windows Server receives updates differ from Windows Home and Pro? Windows Server can disable updates entirely through group policy (as can Windows 10 Enterprise and Windows LTSB).
+Does Windows Server have different hardware requirements than Windows Home or Pro? yes because of the purpose of the system 
