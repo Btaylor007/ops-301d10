7b8351dd50918cf5da1d5ee7b7569cab1aaa4363
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-12 10:47:28 -0600

    read12.sh

---
+++ read12.sh
@@ -0,0 +1,17 @@
+Readings: Domain Controller
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+What is a Windows Domain and How Does It Affect My PC?
+
+Explain the role of a Domain Controller?ou control the settings and user accounts on the computer. A computer joined to a domain is different -- these settings are controlled on a domain controller.
+What is the benefit of being able to login with the same username and password on any computer joined to the domain? What are the security risks?
+Describe how group policies are used in domains?they are used to regulate users in an active directory 
+In what other ways can you think of that domains could be used beyond what was presented in the reading?
+Skim
+Top 10 Dangerous DNS Attacks Types and The Prevention Measures
+Videos
+Overview of DNS
+DNS Record Types
