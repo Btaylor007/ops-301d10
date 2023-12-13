151571d9dc17298618f9de91621dc248021f0c6b
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-13 10:39:39 -0600

    read13.sh

---
+++ read13.sh
@@ -0,0 +1,21 @@
+Readings: Active Directory
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+What is Active Directory?
+
+What exactly is “Active Directory” and are the key services it provides?t is used to authenticate users and to control access to network resources. A server running AD DS is called a domain controller. Most Windows domain networks have two or more domain controllers; a primary domain controller and one or more backup domain controllers for resiliency.
+
+What are the differences between a domain, forest, and tree in Active Directory?A domain is a collection of objects (e.g. users, devices) that share the same Active Directory database. A domain is identified by a DNS name like company.com.
+A tree is a collection of one or more domains with a contiguous namespace (they have a common DNS root name like marketing.company.com, engineering.company.com, and sales.company.com).
+A forest is a collection of one or more trees that share a common schema, global catalog, and directory configuration—but aren’t part of a contiguous namespace. The forest typically serves as the security boundary for an enterprise network.
+
+How can objects (e.g. users, devices) within a domain be grouped?
+
+Explain the benefits of Active Directory, as you would to a family member.
+
+Videos
+DHCP Overview
+Configuring DHCP
