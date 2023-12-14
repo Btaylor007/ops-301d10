5ca2e5f5fedde6c74bb342b97ae45399b028f0c7
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-14 10:53:07 -0600

    read14.sh

---
+++ read14.sh
@@ -0,0 +1,15 @@
+Readings: Group Policy
+Below you will find reading material and additional resources that support today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+What is Group Policy and What Role Does It Play in Data SecurityGroup Policy plays a crucial role in Windows Active Directory (AD) as a powerful tool for centrally managing and enforcing policies on users and computers across your network. It functions like a digital rulebook, dictating various aspects of their environment and behavior.
+
+What role does Group Policy play in Windows Active Directory? 
+Name and describe different ways GPOs can benefit security.Restrict user privileges,Block unauthorized applications,Encrypt sensitive data,Reduced human error
+How can the acronym “LSDOU” help you figure out which policies are in effect? 
+VideosThe acronym LSDOU stands for Local Settings, Site, Domain, Organizational Unit. It represents the four levels in Active Directory where Group Policy Objects (GPOs) can be applied, and it can indeed help you determine which policies are in effect for a specific computer or user.
+Wireless Standards
+Wireless Technologies
+Wireless Encryption
