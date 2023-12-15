efc465e68b307d95ae1ea7d3b5949b6499be35bd
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-14 23:14:55 -0600

    diversity read.sh

---
+++ diversity read.sh
@@ -0,0 +1,16 @@
+Diversity & Inclusion in the Tech Industry
+Below you will find some reading material that supports today’s topic and the upcoming lecture.
+
+Review the Submission Instructions for guidance on completing and submitting this assignment.
+
+Reading
+Consider the history: That Time When Women Stopped Coding
+What occurred during the same time as the beginning of the decline of women in computer science? Cultural factors,Changes in education, Gender stereotypes,Workplace culture
+Why does it matter that males had been playing on computers growing up? Society puts males in a position to grow in the field based of old ways that are illogical thoughts 
+Review the data: Employee breakdown of key technology companies 
+
+Ask the question: Why diversity matters to your tech company
+
+When are diversity efforts most successful? Diversity is successful when everyone feels included and there is a group that has a respectful, understanding and dynamic.
+Why do diverse companies perform better? Diversity in companies provides a wide variety of ideas, great employees with a good work ethic that in return helps the company to succeed.
+Give an example of how a diverse company can serve a diverse user base or vise-versa. A clothing is what I would consider to be a good example of a diverse user base.
