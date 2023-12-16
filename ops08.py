3a1008122c853fab375c4f9b9a15aed01212fb11
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-16 15:23:19 -0600

    ops08.py

--- ops08.py
+++ ops08.py
@@ -12,3 +12,13 @@ fruits[6] = "onion"
 
 # Print the modified list
 print(fruits)  # output: ['apple', 'banana', 'cherry', 'orange', 'mango', 'grapefruit', 'onion', 'melon', 'pineapple', 'strawberry']
+
+# Print the sixth through tenth element using slicing
+print("Elements 6 to 10:")
+for fruit in fruits[5:]:
+    print(f"\t- {fruit}")
+
+# Print the updated list (optional)
+print("\nUpdated list:")
+for fruit in fruits:
+    print(fruit)
