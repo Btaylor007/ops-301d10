2ff0110f9fd5113f2289cfa88cf088c5bccf264c
Breanna Taylor <147112725+Btaylor007@users.noreply.github.com>
2023-12-16 21:01:15 -0600

    read07.md

--- read07.sh
+++ read07.sh
@@ -4,13 +4,15 @@ Below you will find reading material and additional resources that support today
 Review the Submission Instructions for guidance on completing and submitting this assignment.
 
 Reading
-What is NGINX?
-
-What are some common use cases for NGINX?
-How does NGINX handle tasks that could slow down the web server?
+What is NGINX? a versatile software that plays several roles in the world of websites and online services.
+What are some common use cases for NGINX? website servers, traffic security, 
+How does NGINX handle tasks that could slow down the web server?  NGINX tackles slowdowns through clever traffic management, resource-efficient caching, optimized operations, and robust security. It's like a well-oiled machine working behind the scenes to keep your website humming along at peak performance
 Describe, as if to a non-technical friend how to actually pronounce “NGINX”, and why an org might chooose to use it.
 Videos
 Network Architectures
 Network Devices
 Network Connectors
 Ethernet Standards
+resources 
+https://www.nginx.com/resources/glossary/nginx/
+bard.com 
